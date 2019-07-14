e4aa8da496753724515c8b0026e8c66ab030f66a
mattdocherty314 <mattdocherty314@gmail.com>
2019-07-15 09:00:37 +1000

    Made routes list, to easily add or remove them

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var routeDirs = ["/", "/projects", "/resume"];
 
 var app = express();
 
@@ -19,8 +18,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+routeDirs.forEach(dir => {
+  var importRoute = (dir === "/") ? "/routes/index" : `/routes/${dir}`;  
+  app.use(`${dir}`, require(importRoute));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
