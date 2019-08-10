ceb31d282404bdfa33f2ee806f72c62e3c8180ab
mattdocherty314 <mattdocherty314@gmail.com>
2019-08-11 08:25:22 +1000

    Made Mongo Interface

--- backend/database/db-interface.js
+++ backend/database/db-interface.js
@@ -1,1 +1,42 @@
-var mongo = require('mongo');
+var fs = require('fs');
+var mongo = require('mongodb').MongoClient;
+
+function queryData(dbCollection, query) {
+    const config = fs.readFileSync('./mongo-config.js');
+    return mongo.connect(`mongodb://${config.host}:${config.port}/`, (err, db) => {
+        let database = db.db(config.database);
+        return database.collection(dbCollection).find(query).toArray((err, res) => {
+            return res
+        });
+    });
+}
+
+function insertData(dbCollection, data) {
+    const config = fs.readFileSync('./mongo-config.js');
+    return mongo.connect(`mongodb://${config.host}:${config.port}/`, (err, db) => {
+        let database = db.db(config.database);
+        return database.collection(dbCollection).insertMany(data).toArray((err, res) => {
+            return res
+        });
+    });
+}
+
+function deleteData(dbCollection, query) {
+    const config = fs.readFileSync('./mongo-config.js');
+    return mongo.connect(`mongodb://${config.host}:${config.port}/`, (err, db) => {
+        let database = db.db(config.database);
+        return database.collection(dbCollection).deleteMany(query).toArray((err, res) => {
+            return res
+        });
+    });
+}
+
+function updateData(dbCollection, query, data) {
+    const config = fs.readFileSync('./mongo-config.js');
+    return mongo.connect(`mongodb://${config.host}:${config.port}/`, (err, db) => {
+        let database = db.db(config.database);
+        return database.collection(dbCollection).updateMany(query, data).toArray((err, res) => {
+            return res
+        });
+    });
+}
