4816741269e0e3e2da3760c11308331d33b3ea25
mattdocherty314 <mattdocherty314@gmail.com>
2019-08-11 07:56:43 +1000

    Started on Mongo Database Interface

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var dbInterface = require('./db-interface.js')
 
+var dbInterface = require('./database/db-interface.js')
 var routeDirs = ["/", "/projects", "/resume"];
 
 var app = express();
