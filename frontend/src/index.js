740b15ee47a7dcbb30e6819040e9c9a84f4df4dc
mattdocherty314 <mattdocherty314@gmail.com>
2019-11-20 11:46:26 +1000

    Made React Components and Routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
+import { Projects } from './routes/projects-route';
+import { Resume } from './routes/resume-route';
+
 function App() {
     return (
         <Router>
-
+            <div className="App">
+                <aside>
+                    <Link to={"/projects"}>Projects</Link>
+                    <Link to={"/resume"}>Resume</Link>
+                </aside>
+                <main>
+                    <Route exact path="/" component={Menu} />
+                    <Route path="/projects" component={Projects} />
+                    <Route path="/resume" component={Resume} />
+                </main>
+            </div>
         </Router>
+    )
+}
+
+function Menu() {
+    return (
+        <h1> Matthew Docherty </h1>
     )
 }
 
