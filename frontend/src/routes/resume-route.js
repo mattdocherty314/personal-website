9555a22793bc72633e95ff8698af57af17fde4ba
mattdocherty314 <mattdocherty314@gmail.com>
2019-11-20 11:46:44 +1000

    Started on Website Resume

---
+++ frontend/src/routes/resume-route.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+export function Resume() {
+    return (
+        <div id="resume">
+            <h1> Resume </h1>
+            
+            <h2> Education </h2>
+            <p>
+                <em> Bachelor of Engineering (Honours) / Information Technology </em><br/>
+                Queensland University of Technology <br/>
+                Jan 2018 - Est. Nov 2022 <br/>
+                <table>
+                <tr>
+                        <td><strong> Current GPA: </strong></td>
+                        <td> 5.917 </td>
+                    </tr>
+                    <tr>
+                        <td><strong> Engineering (Honours) Major: </strong></td>
+                        <td> Computer and Software Systems </td>
+                    </tr>
+                    <tr>
+                        <td><strong> Information Technology Major:&emsp;&emsp;&emsp; </strong></td>
+                        <td> Computer Science </td>
+                    </tr>
+                    <tr>
+                        <td><strong> Electives: </strong></td>
+                        <td> 
+                            <li> CAB230: Web Computing </li>
+                            <li> CAB240: Information Security </li>
+                        </td>
+                    </tr>
+                </table>
+            </p>
+        </div>
+    )
+}
