2a721a2e6215d00cea91befc5a52f572612449f3
mattdocherty314 <mattdocherty314@gmail.com>
2019-11-20 10:37:31 +1000

    Cleaned Up Default React App & Updated NPM

--- backend/routes/resume.js
+++ backend/routes/resume.js
@@ -1,9 +1,71 @@
 var express = require('express');
 var router = express.Router();
 
+var dbInterface = require('../database/db-interface.js');
+
 /* GET resume listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  let requestQuery = req.query;
+  
+  let query = createQuery(requestQuery);
+  let dbQuery = createMongoQuery(query);
+  
+  sendToDB(dbQuery)
+  .then((dbRes, err) => {
+    res.setHeader('Content-Type', 'application/json');
+    console.log(dbRes)
+    res.send(dbRes);
+  })
 });
+
+function createQuery(userRequest) {
+  let defQuery = {
+    'page': 1,
+    'numPerPage': 4
+  }
+
+  for (let setting in defQuery) {
+    for (let userSetting in userRequest) {
+      if (setting === userSetting) {
+        let parsedSetting = parseStringToCorrect(userRequest[userSetting]);
+        defQuery[setting] = parsedSetting;
+        break;
+      }
+    }
+  }
+
+  return defQuery;
+}
+
+function parseStringToCorrect(stringToParse) {
+  var parsedItem = null;
+  
+  parsedItem = parseInt(stringToParse);
+  if (!isNaN(parsedItem)) {
+    return parsedItem;
+  }
+  
+  if (stringToParse === "true") {
+    return true;
+  } else if (stringToParse === "false") {
+    return false;
+  }
+
+  return stringToParse;
+}
+
+function createMongoQuery(settings) {
+  let mongoQuery = {
+    id: {
+      $gte: (settings.page-1) * settings.numPerPage,
+      $lte: settings.page * settings.numPerPage - 1
+    }
+  };
+  return mongoQuery;
+}
+
+function sendToDB(query) {
+  return dbInterface.queryData('resume', query);
+}
 
 module.exports = router;
