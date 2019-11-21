9ff9db3fcefd8958c717f3d7f061217d4ad0703a
mattdocherty314 <mattdocherty314@gmail.com>
2019-11-21 12:18:53 +1000

    Transferred Rest of Resume to Component

--- frontend/src/routes/resume-route.js
+++ frontend/src/routes/resume-route.js
@@ -7,8 +7,8 @@ export function Resume() {
             
             <h2> Education </h2>
             <p>
-                <em> Bachelor of Engineering (Honours) / Information Technology </em><br/>
-                Queensland University of Technology <br/>
+                <strong> Bachelor of Engineering (Honours) / Information Technology </strong><br/>
+                <em> Queensland University of Technology </em> <br/>
                 Jan 2018 - Est. Nov 2022 <br/>
                 <table>
                 <tr>
@@ -20,15 +20,71 @@ export function Resume() {
                         <td> Computer and Software Systems </td>
                     </tr>
                     <tr>
-                        <td><strong> Information Technology Major:&emsp;&emsp;&emsp; </strong></td>
+                        <td><strong> Information Technology Major:</strong></td>
                         <td> Computer Science </td>
                     </tr>
                     <tr>
                         <td><strong> Electives: </strong></td>
                         <td> 
-                            <li> CAB230: Web Computing </li>
-                            <li> CAB240: Information Security </li>
+                            <li> CAB230: Web Computing (GPA 6) </li>
+                            <li> CAB240: Information Security (GPA tbd) </li>
                         </td>
+                    </tr>
+                </table>
+            </p>
+
+            <h2> Experience </h2>
+            <p>
+                <strong> School IT Technician </strong> <br/>
+                <em> Mansfield State High School </em> <br/>
+                Feb 2018 - Apr 2018, Dec 2018 - Oct 2019 <br/>
+                As a school IT technician, I and other senior technicians were
+                responsible for resolving issues relating to the school computers,
+                laptops and related techologies. For example, if a projector wasn't
+                connecting to a teacher computer, I had to diagnose whether it
+                was the cable or if the computer wasn't configured for the
+                projector. <br/>
+                The primary thing that I learnt from the job was that I don't
+                have to always know the answer to problems. I also learn't how
+                to effectively communicate the problem to co-workers so that they
+                can suggest solutions that weren't attempted. <br/>
+                <table>
+                    <tr>
+                        <td> <strong> Reference: </strong> </td>
+                        <td> Ben Agnew, Senior School IT Technician </td>
+                        <td> *@*.* </td>
+                        <td> #* </td>
+                    </tr>
+                </table> 
+            </p>
+
+            <h2> Skills &amp; Qualities </h2>
+            <ul>
+                <li> Problem Solver / Logical Thinker </li>
+                <li> Hard &amp; Determined Thinker </li>
+                <li> Team Player </li>
+                <li> IT &amp; Programming Skills </li>
+            </ul>
+
+            <h2> Activities </h2>
+            <p>
+                <stong> Leadership &amp; Volunteering </stong>
+                <ul>
+                    <li> SU QLD Youth Leadership </li>
+                    <li> QUT Student COnnect Leader </li>
+                    <li> CoderDojo Mentor Volunteer </li>
+                    <li> Gateway Baptist Coffee Shop Volunteer </li>
+                </ul>
+
+                <strong> Certifications &amp; Awards </strong>
+                <table>
+                    <tr>
+                        <td> GovHack 2017: </td>
+                        <td> <em> Honourable Mention Award </em> </td>
+                    </tr>
+                    <tr>
+                        <td> QUT Sem 1 2018: </td>
+                        <td> <em> Dean's Honour Award </em> </td>
                     </tr>
                 </table>
             </p>
