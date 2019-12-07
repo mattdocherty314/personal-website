2a8039ca107f0e64935e01dba90e0c4d422a5b77
mattdocherty314 <mattdocherty314@gmail.com>
2019-12-07 10:02:42 +1000

    Fixed DB Interface

--- backend/database/db-interface.js
+++ backend/database/db-interface.js
@@ -10,36 +10,39 @@ module.exports = {
         })
         .then((database, err) => {
             return database.collection(dbCollection).find(query).toArray();
-        })
+        });
     },
 
     insertData: function(dbCollection, data) {
-        const config = fs.readFileSync('./database/mongo-config.json');
-        return mongo.connect(`mongodb://${config.host}:${config.port}/`, (err, db) => {
-            let database = db.db(config.database);
-            return database.collection(dbCollection).insertMany(data).toArray((err, res) => {
-                return res;
-            });
+        const config = JSON.parse(fs.readFileSync('./database/mongo-config.json'));
+        return mongo.connect(`mongodb://${config.host}:${config.port}/`)
+        .then((db, err) => {
+            return db.db(config.database);
+        })
+        .then((database, err) => {
+            return database.collection(dbCollection).insertMany(data).toArray();
         });
     },
 
     deleteData: function(dbCollection, query) {
-        const config = fs.readFileSync('./database/mongo-config.json');
-        return mongo.connect(`mongodb://${config.host}:${config.port}/`, (err, db) => {
-            let database = db.db(config.database);
-            return database.collection(dbCollection).deleteMany(query).toArray((err, res) => {
-                return res;
-            });
+        const config = JSON.parse(fs.readFileSync('./database/mongo-config.json'));
+        return mongo.connect(`mongodb://${config.host}:${config.port}/`)
+        .then((db, err) => {
+            return db.db(config.database);
+        })
+        .then((database, err) => {
+            return database.collection(dbCollection).deleteMany(query).toArray();
         });
     },
 
     updateData: function(dbCollection, query, data) {
-        const config = fs.readFileSync('./database/mongo-config.json');
-        return mongo.connect(`mongodb://${config.host}:${config.port}/`, (err, db) => {
-            let database = db.db(config.database);
-            return database.collection(dbCollection).updateMany(query, data).toArray((err, res) => {
-                return res;
-            });
-        });
+        const config = JSON.parse(fs.readFileSync('./database/mongo-config.json'));
+        return mongo.connect(`mongodb://${config.host}:${config.port}/`)
+        .then((db, err) => {
+            return db.db(config.database);
+        })
+        .then((database, err) => {
+            return database.collection(dbCollection).updateMany(data).toArray();
+        })
     }
 };
