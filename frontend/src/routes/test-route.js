d5d17eccb82193bb89595a5a3917c6b28cf71357
Matthew Docherty <mattdocherty314@gmail.com>
2019-12-21 17:48:02 +1000

    Added a 'test' route for No Drop Plugin

--- frontend/src/routes/test-route.js
+++ frontend/src/routes/test-route.js
@@ -3,7 +3,8 @@ import React from 'react';
 export function Test() {
     let impl = [
         "encrypt-decrypt", "genetic-evolution", "game-of-life", "3d-render",
-        "2d-shader", "day-planner", "data-search", "remote-script", "png-to-mc-blocks"
+        "2d-shader", "day-planner", "data-search", "remote-script", "png-to-mc-blocks",
+        "no-drop-plugin"
     ];
     for (let proj = 0; proj < impl.length; proj++) {
         if (window.location.href.indexOf(impl[proj]) !== -1) {
