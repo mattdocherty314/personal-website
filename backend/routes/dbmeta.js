3acb9f4ff37f291e5ace498bb200b37da4b3b217
Matthew Docherty <mattdocherty314@gmail.com>
2020-03-09 09:15:53 +1000

    Fixed comment

--- backend/routes/dbmeta.js
+++ backend/routes/dbmeta.js
@@ -5,11 +5,12 @@ var jwt = require('jsonwebtoken');
 var dbInterface = require('../database/db-interface.js');
 var jwtSecret = require('../jwt.js');
 
-/* GET dbmeta listing. */
+/* POST dbmeta listing. */
 router.post('/', function(req, res, next) {
   let token = jwt.verify(req.headers.authorization.split(" ")[1], jwtSecret.value);
-  
+
   if (token.data.access === "admin") {
+    
     sendToDB({})
     .then((dbRes, err) => {
       res.setHeader('Content-Type', 'application/json');
