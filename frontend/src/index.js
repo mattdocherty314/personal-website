ae70927467c984cc915bfba90a7a71373d1a3e7f
Matthew Docherty <mattdocherty314@gmail.com>
2021-01-26 14:14:18 +1000

    Added a 404 page on frontend.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 import { Projects } from './routes/projects-route';
 import { Resume } from './routes/resume-route';
@@ -8,6 +8,7 @@ import { Test } from './routes/test-route';
 import { Login } from './routes/login-route';
 import { Contact } from './routes/contact-route';
 import { Servers } from './routes/servers-route';
+import { Error } from './routes/error-route';
 import './index.css'
 
 function App() {
@@ -21,7 +22,7 @@ function App() {
 					<Link to={"/contact"}><li>Contact</li></Link>
                     <Link to={"/servers"}><li>Servers</li></Link>
                 </ul></aside>
-                <main>
+                <Switch>
                     <Route exact path="/" component={Menu} />
                     <Route path="/projects" component={Projects} />
                     <Route path="/resume" component={Resume} />
@@ -29,7 +30,8 @@ function App() {
                     <Route path="/login" component={Login} />
 					<Route path="/contact" component={Contact} />
                     <Route path="/servers" component={Servers} />
-                </main>
+					<Route component={Error} />
+                </Switch>
             </div>
         </Router>
     )
